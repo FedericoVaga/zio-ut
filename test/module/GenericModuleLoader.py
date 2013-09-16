9f9e2331c453d6320ceb9d20f4614a1bbfe7f333
Federico Vaga <federico.vaga@gmail.com>
2013-09-16 17:52:04 +0200

    module.GenericModuleLoader: fix style and copyright
    Signed-off-by: Federico Vaga <federico.vaga@gmail.com>

--- test/module/GenericModuleLoader.py
+++ test/module/GenericModuleLoader.py
@@ -1,6 +1,6 @@
 """
 @author: Federico Vaga <federico.vaga@gmail.com>
-@copyright: Federico Vaga 2012
+@copyright: CERN 2013
 @license: GPLv2
 """
 from test import config
@@ -20,10 +20,12 @@ class GenericModuleLoader:
         time.sleep(config.wait_insmod_rmmod)
         self._test_rmmod(module_name)
 
+
     def _test_insmod(self, module_name):
         err = self._do_insmod(module_name)
         self.assertEqual(err, "", "insmod error: " + err)
-    
+
+
     def _test_rmmod(self, module_name):
         err = self._do_rmmod(module_name)
         self.assertEqual(err, "", "rmmod error: " + err)
@@ -31,23 +33,24 @@ class GenericModuleLoader:
 
     def _do_insmod(self, module_name):
         cmd = "insmod " + module_name + ".ko"
-        p = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE,\
-                     stdout=subprocess.PIPE, stderr=subprocess.PIPE,\
-                     close_fds=True)
+        p = subprocess.Popen(cmd, shell = True, stdin = subprocess.PIPE, \
+                     stdout = subprocess.PIPE, stderr = subprocess.PIPE, \
+                     close_fds = True)
         return p.stderr.read()
-        
+
 
     def _do_rmmod(self, module_name):
         cmd = "rmmod " + module_name
-        p = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE,\
-                     stdout=subprocess.PIPE, stderr=subprocess.PIPE,\
-                     close_fds=True)
+        p = subprocess.Popen(cmd, shell = True, stdin = subprocess.PIPE, \
+                     stdout = subprocess.PIPE, stderr = subprocess.PIPE, \
+                     close_fds = True)
         return p.stderr.read()
+
 
     def _get_all_modules(self):
         cmd = "ls zio*.ko"
-        p = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE,
-                             stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
-                             close_fds=True)
+        p = subprocess.Popen(cmd, shell = True, stdin = subprocess.PIPE, \
+                     stdout = subprocess.PIPE, stderr = subprocess.STDOUT, \
+                     close_fds = True)
         out = p.stdout.read()
         return out.split()
