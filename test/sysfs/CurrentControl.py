947d7cb75d905131cb8d3c9dddb97e33cdaeb491
Federico Vaga <federico.vaga@gmail.com>
2014-01-09 18:25:50 +0100

    sysfs/CurrentControl: add propagation test sysfs -> cur ctrl
    Signed-off-by: Federico Vaga <federico.vaga@gmail.com>

--- test/sysfs/CurrentControl.py
+++ test/sysfs/CurrentControl.py
@@ -76,3 +76,68 @@ class CurrentControl(unittest.TestCase):
                     "Last control (block) must be equal to current-control")
 
             ctrl_prev = ctrl_cdev
+
+    def test_from_attr_to_cur_ctrl(self):
+        """
+        It tests that user value written on a trigger sysfs attribute will be
+        written also on the current control.
+        """
+        self.cset = self.device.cset[2]  # Use cset input32
+        self.chan = self.cset.chan[0]
+        test_attr = (("resolution-bits", 0),
+                     ("gain_factor", 1),
+                     ("offset", 2),
+                     ("max-sample-rate", 3),
+                     ("vref-src", 4),
+                     ("nshots", 0),
+                     ("post-samples", 1),
+                     ("pre-samples", 2)
+                    )
+
+        for attr_name, attr_idx in test_attr:
+            # Find the attribute within the zio device hierachy
+            in_trig = False
+            if attr_name in self.device.attribute:
+                attr = self.device.attribute[attr_name]
+            elif attr_name in self.cset.attribute:
+                attr = self.cset.attribute[attr_name]
+            elif attr_name in self.chan.attribute:
+                attr = self.chan.attribute[attr_name]
+            elif attr_name in self.trigger.attribute:
+                attr = self.trigger.attribute[attr_name]
+                in_trig = True
+            else:
+                continue
+
+            # Do no continue if the attribute is read-only
+            if not attr.is_writable():
+                continue
+
+            print("    Test attribute '{0}'".format(attr_name))
+
+            # Do some writes
+            increment = 1
+            for _i in range(config.nstress):
+                new_val = 2 + increment
+                attr.set_value(new_val)
+                self._compare_with_cur_ctrl(self.cset, attr_idx, new_val, in_trig)
+                increment = -1 if increment == 1 else 1
+
+
+    def _compare_with_cur_ctrl(self, cset, index, value, in_trig):
+        """
+        It test that a given value is equal to the value in current control
+        associated to a given index
+
+        value == cur_ctrl.attribute[index]
+        """
+        # Read the channel current control
+        for chan in cset.chan:
+            cur_ctrl = chan.get_current_ctrl()
+            if in_trig:
+                cur_ctrl_val = cur_ctrl.attr_trigger.std_val[index]
+            else:
+                cur_ctrl_val = cur_ctrl.attr_channel.std_val[index]
+
+            self.assertEqual(value, cur_ctrl_val,
+                "Sysfs attribute '{0}' and current control value '{1}'".format(value, cur_ctrl_val))
