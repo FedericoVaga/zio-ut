e400e2f7987e9547d30910d0859a3d7cf13f5b3c
Federico Vaga <federico.vaga@gmail.com>
2014-01-21 11:11:12 +0100

    setup: add environment variables
    Signed-off-by: Federico Vaga <federico.vaga@gmail.com>

--- test/setup.py
+++ test/setup.py
@@ -12,6 +12,12 @@ def parse_environment():
     config.nstress = _set_variable(config.nstress, "nstress")
     config.nrandom = int(_set_variable(config.nrandom, "nrandom"))
 
+    config.timer_ms_period = int(_set_variable(config.timer_ms_period, \
+                                               "timer_ms_period"))
+    config.acquisition_wait = int(_set_variable(config.acquisition_wait, \
+                                                "acquisition_wait"))
+    config.hrt_slack_nsec = int(_set_variable(config.hrt_slack_nsec, \
+                                                "hrt_slack_nsec"))
 
 def _set_variable(var, name):
     print("looking for {0}".format(name))
